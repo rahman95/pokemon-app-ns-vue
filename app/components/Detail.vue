41b71a4497f743d5647dd98b0bd88da87496a588
Rahman Younus <rahman@industrious.agency>
2018-11-30 18:09:47 +0000

    updated

---
+++ app/components/Detail.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'Detail',
+
+    components: {
+      //
+    },
+
+    data() {
+      return {
+        //
+      }
+    },
+
+    computed: {
+      //
+    },
+
+    methods: {
+
+    },
+  }
+</script>
+
+<style lang="scss">
+</style>
---
+++ app/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <Page ref="page" :class="pageClasses" actionBarHidden="true" backgroundSpanUnderStatusBar="true">
+    <GridLayout row="0" columns="44, *, auto" class="action-bar-custom">
+      <Label col="1" text="Test"></Label>
+    </GridLayout>
+  </Page>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    components: {
+      //
+    },
+
+    data() {
+      return {
+        //
+      }
+    },
+
+    computed: {
+      //
+    },
+
+    methods: {
+
+    },
+  }
+</script>
+
+<style lang="scss">
+</style>
